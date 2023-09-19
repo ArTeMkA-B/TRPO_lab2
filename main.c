76d77beefdfc78c9e537aec3a8addfe58ab5093b
ArTeMkA-B <artem031201@yandex.ru>
2023-09-19 16:29:54 +0300

    Change return status

--- main.c
+++ main.c
@@ -5,5 +5,5 @@ int main(int argc, char **argv)
  char first[255], last[255];
  askname(first, last);
  printf("Hello, %s %s!\n ArTeMkA-BaBiN is the second!!!", first, last);
- return 0;
+ return 1;
 }
