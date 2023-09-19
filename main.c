c2c78e35b273bb1341051db9af1605525a323f23
mikhail-xnor <mikhail-xnor@yandex.ru>
2023-09-19 16:34:23 +0300

    Fix error

--- main.c
+++ main.c
@@ -5,5 +5,5 @@ int main(int argc, char **argv)
  char first[255], last[255];
  askname(first, last);
  printf("Hello, %s %s!\n ArTeMkA-BaBiN is the second!!!", first, last);
- return 1;
+ return 0;
 }
